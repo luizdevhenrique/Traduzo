5061dc1b46394e5f3afdaed0d56fb09c72e32e81
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-26 15:45:19 -0300

    requisito 8 feito

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,26 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response = HistoryModel.list_as_json()
+    data = json.loads(response)
+
+    expected_data = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+        ]
+
+    for item in data:
+        item.pop("_id", None)
+
+    assert data == expected_data, f"Expected {expected_data}, but got {data}"
